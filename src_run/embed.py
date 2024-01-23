bcb23df66e221afeb01cbfafec49dfb9cb4263f6
Ron Boger <ronboger@gmail.com>
2024-01-23 15:01:38 -0800

    creates embed script

---
+++ src_run/embed.py
@@ -0,0 +1,138 @@
+import argparse
+import numpy as np
+import pandas as pd
+import torch
+from model_protein_moe import trans_basic_block, trans_basic_block_Config
+from utils_search import *
+from transformers import T5EncoderModel, T5Tokenizer
+import gc
+import numpy as np
+import pandas as pd
+from collections import defaultdict
+import faiss
+from Bio import SeqIO
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
+def fasta_to_csv(input_fasta, output_csv):
+    # Read the .fasta file
+    records = SeqIO.parse(input_fasta, "fasta")
+
+    # Create a list to store the data
+    data = []
+
+    # Extract the Accession and Sequence from each record
+    for record in records:
+        accession = record.id
+        sequence = str(record.seq)
+        data.append([accession, sequence])
+
+    # Create a DataFrame from the data
+    df = pd.DataFrame(data, columns=["Accession", "Sequence"])
+
+    # Save the DataFrame as a CSV file
+    if output_csv is not None:
+        df.to_csv(output_csv, index=False)
+
+    return df
+
+
+def load_vec_model(vec_model_cpnt, vec_model_config):
+    # Load the model
+    vec_model_config = trans_basic_block_Config.from_json(vec_model_config)
+    model_deep = trans_basic_block.load_from_checkpoint(
+        vec_model_cpnt, config=vec_model_config
+    )
+    model_deep = model_deep.to(device)
+    model_deep = model_deep.eval()
+    return model_deep
+
+
+def get_tokenizer(ProtTrans_model, ProtTrans_tokenizer):
+    # Load the ProtTrans model and ProtTrans tokenizer
+    tokenizer = T5Tokenizer.from_pretrained(ProtTrans_tokenizer, do_lower_case=False)
+    model = T5EncoderModel.from_pretrained(ProtTrans_model)
+    gc.collect()
+
+    model = model.to(device)
+    model = model.eval()
+    return model, tokenizer
+
+
+def get_masks():
+    # This is a forward pass of the Protein-Vec model
+    # Every aspect is turned on (therefore no masks)
+    sampled_keys = np.array(["TM", "PFAM", "GENE3D", "ENZYME", "MFO", "BPO", "CCO"])
+    all_cols = np.array(["TM", "PFAM", "GENE3D", "ENZYME", "MFO", "BPO", "CCO"])
+    masks = [all_cols[k] in sampled_keys for k in range(len(all_cols))]
+    masks = torch.logical_not(torch.tensor(masks, dtype=torch.bool))[None, :]
+
+
+def load_data(input_csv, input_fasta, output_csv):
+    # Load the data
+    if input_csv is not None:
+        df = pd.read_csv(input_csv)
+
+    # If the data is not already in a CSV file, convert the .fasta file to a CSV file
+    if input_fasta is not None:
+        df = fasta_to_csv(input_fasta, output_csv)
+
+    return df
+
+
+def main():
+    df = load_data(args.input_csv, args.input_fasta, args.output_csv)
+
+    model, tokenizer = get_tokenizer(args.ProtTrans_model, args.ProtTrans_tokenizer)
+    model_deep = load_vec_model(args.vec_model_cpnt, args.vec_model_config)
+
+    masks = get_masks()
+
+    embeddings = encode(
+        df["Sequence"].values, model_deep, model, tokenizer, masks, device
+    )
+    # save the embeddings
+    np.save(args.output_file, embeddings)
+
+
+if __name__ == "__main__":
+    # parse arguments
+    parser = argparse.ArgumentParser(description="Embedding generation")
+    parser.add_argument("--input_fasta", type=str, help="input fasta file")
+    parser.add_argument("--input_csv", type=str, help="output csv file", default=None)
+    parser.add_argument("--output_csv", type=str, help="output csv file", default=None)
+    parser.add_argument(
+        "--vec_model_cpnt",
+        type=str,
+        help="vec model checkpoint",
+        default="protein_vec_models/protein_vec.ckpt",
+    )
+    parser.add_argument(
+        "--vec_model_config",
+        type=str,
+        help="vec model config",
+        default="protein_vec_models/protein_vec_params.json",
+    )
+    parser.add_argument(
+        "--ProtTrans_model",
+        type=str,
+        help="ProtTrans model",
+        default="Rostlab/prot_t5_xl_uniref50",
+    )
+    parser.add_argument(
+        "--ProtTrans_tokenizer",
+        type=str,
+        help="ProtTrans tokenizer",
+        default="Rostlab/prot_t5_xl_uniref50",
+    )
+    parser.add_argument(
+        "--output_file",
+        type=str,
+        help="output file for embeddings",
+        default="embeddings.npy",
+    )
+
+    args = parser.parse_args()
+
+    main(args)
--- src_run/utils_search.py
+++ src_run/utils_search.py
@@ -11,62 +11,68 @@ from torch import nn
 from torch.utils.data import DataLoader
 import faiss
 import os
-
+from tqdm import tqdm
 
 
 def load_database(lookup_database):
-    #Build an indexed database
+    # Build an indexed database
     d = lookup_database.shape[1]
     index = faiss.IndexFlatIP(d)
     faiss.normalize_L2(lookup_database)
     index.add(lookup_database)
 
-    return(index)
+    return index
 
 
 def query(index, queries, k=10):
     faiss.normalize_L2(queries)
     D, I = index.search(queries, k)
 
-    return(D, I)
+    return (D, I)
 
-def featurize_prottrans(sequences, model, tokenizer, device): 
-    
+
+def featurize_prottrans(sequences, model, tokenizer, device):
     sequences = [(" ".join(sequences[i])) for i in range(len(sequences))]
     sequences = [re.sub(r"[UZOB]", "X", sequence) for sequence in sequences]
     ids = tokenizer.batch_encode_plus(sequences, add_special_tokens=True, padding=True)
-    input_ids = torch.tensor(ids['input_ids']).to(device)
-    attention_mask = torch.tensor(ids['attention_mask']).to(device)
+    input_ids = torch.tensor(ids["input_ids"]).to(device)
+    attention_mask = torch.tensor(ids["attention_mask"]).to(device)
 
     with torch.no_grad():
         embedding = model(input_ids=input_ids, attention_mask=attention_mask)
 
     embedding = embedding.last_hidden_state.cpu().numpy()
 
-    features = [] 
+    features = []
     for seq_num in range(len(embedding)):
         seq_len = (attention_mask[seq_num] == 1).sum()
-        seq_emd = embedding[seq_num][:seq_len-1]
+        seq_emd = embedding[seq_num][: seq_len - 1]
         features.append(seq_emd)
-    
+
     prottrans_embedding = torch.tensor(features[0])
     prottrans_embedding = torch.unsqueeze(prottrans_embedding, 0).to(device)
-    
-    return(prottrans_embedding)
+
+    return prottrans_embedding
 
 
 def embed_vec(prottrans_embedding, model_deep, masks, device):
-    padding = torch.zeros(prottrans_embedding.shape[0:2]).type(torch.BoolTensor).to(device)
+    padding = (
+        torch.zeros(prottrans_embedding.shape[0:2]).type(torch.BoolTensor).to(device)
+    )
     out_seq = model_deep.make_matrix(prottrans_embedding, padding)
     vec_embedding = model_deep(out_seq, masks)
-    return(vec_embedding.cpu().detach().numpy())
+    return vec_embedding.cpu().detach().numpy()
 
-def encode(sequences, model_deep, model, tokenizer, masks, device):
-    i = 0
-    embed_all_sequences=[]
-    while i < len(sequences):
-        protrans_sequence = featurize_prottrans(sequences[i:i+1], model, tokenizer, device)
+
+def encode(sequences, model_deep, model, tokenizer, masks, device, batch_size=50):
+    embed_all_sequences = []
+    num_seqs = len(sequences)
+    print(num_seqs)
+    for i in tqdm(range(0, num_seqs, batch_size)):
+        j = min(i + batch_size, num_seqs)
+        protrans_sequence = featurize_prottrans(
+            sequences[i:j], model, tokenizer, device
+        )
         embedded_sequence = embed_vec(protrans_sequence, model_deep, masks, device)
         embed_all_sequences.append(embedded_sequence)
-        i = i + 1
     return np.concatenate(embed_all_sequences, axis=0)
